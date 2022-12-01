2cc06b0d6efdd9dcafe4760f60a6242859945425
Kristen Tidmuss <N0961364@my.ntu.ac.uk>
2022-12-01 21:52:01 +0000

    Make setup commands only run on first start

--- Program.cs
+++ Program.cs
@@ -34,51 +34,55 @@ public class Program
         _Client.Ready += Ready;
         await _Client.LoginAsync(TokenType.Bot, settings.Token);
         await _Client.StartAsync();
-        //Register Plugins
-        var reactionFilter = new ReactionFilter(_Client);
-        var youtube = new Youtube(_Client);
-        var scheduler = new Scheduler(_Client);
         //Block Task
         await Task.Delay(-1);
     }
 
     private async Task Ready()
     {
-        //Register Commands
-        _Service = new InteractionService(_Client.Rest);
-        _Service.Log += Log;
-        await _Service.AddModulesAsync(Assembly.GetEntryAssembly(), _ServiceProvider);
-        await _Service.RegisterCommandsGloballyAsync();
-        _Client.InteractionCreated += async interaction =>
+        if (_FirstStart)
         {
-            var ctx = new SocketInteractionContext(_Client, interaction);
-            await _Service.ExecuteCommandAsync(ctx, _ServiceProvider);
-            //Log command ran
-            var cmd = (SocketSlashCommand)interaction;
-            await Log(new LogMessage(LogSeverity.Info, "Command", 
-                $"Command {cmd.Data.Name} ran by {cmd.User.Username}#{cmd.User.Discriminator} in {cmd.Channel.Name}"));
-        };
-        _Service.SlashCommandExecuted += async (command, context, result) =>
-        {
-            if (!result.IsSuccess)
+            _FirstStart = false;
+            //Register Plugins
+            var reactionFilter = new ReactionFilter(_Client);
+            var youtube = new Youtube(_Client);
+            var scheduler = new Scheduler(_Client);
+            //Register Commands
+            _Service = new InteractionService(_Client.Rest);
+            _Service.Log += Log;
+            await _Service.AddModulesAsync(Assembly.GetEntryAssembly(), _ServiceProvider);
+            await _Service.RegisterCommandsGloballyAsync();
+            _Client.InteractionCreated += async interaction =>
             {
-                if (result.ErrorReason.Contains("was not present in the dictionary.") && command.Name == "timestamp")
+                var ctx = new SocketInteractionContext(_Client, interaction);
+                await _Service.ExecuteCommandAsync(ctx, _ServiceProvider);
+                //Log command ran
+                var cmd = (SocketSlashCommand)interaction;
+                await Log(new LogMessage(LogSeverity.Info, "Command", 
+                    $"Command {cmd.Data.Name} ran by {cmd.User.Username}#{cmd.User.Discriminator} in {cmd.Channel.Name}"));
+            };
+            _Service.SlashCommandExecuted += async (command, context, result) =>
+            {
+                if (!result.IsSuccess)
                 {
-                    await context.Interaction.RespondAsync("Invalid time format. Please use the format `12:00 GMT ` or `01 Jan 2022 12:00 GMT`", ephemeral: true);
-                }
-                else
-                {
-                    if (!context.Interaction.HasResponded)
+                    if (result.ErrorReason.Contains("was not present in the dictionary.") && command.Name == "timestamp")
                     {
-                        await context.Interaction.RespondAsync($"Error: {result.ErrorReason}", ephemeral: true);
+                        await context.Interaction.RespondAsync("Invalid time format. Please use the format `12:00 GMT ` or `01 Jan 2022 12:00 GMT`", ephemeral: true);
                     }
                     else
                     {
-                        await context.Interaction.ModifyOriginalResponseAsync(m => m.Content = $"Error: {result.ErrorReason}");
+                        if (!context.Interaction.HasResponded)
+                        {
+                            await context.Interaction.RespondAsync($"Error: {result.ErrorReason}", ephemeral: true);
+                        }
+                        else
+                        {
+                            await context.Interaction.ModifyOriginalResponseAsync(m => m.Content = $"Error: {result.ErrorReason}");
+                        }
                     }
                 }
-            }
-        };
+            };
+        }
         //Status
         await _Client.SetStatusAsync(UserStatus.DoNotDisturb);
     }
