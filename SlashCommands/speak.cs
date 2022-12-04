3279d4df8076bb4960e26f914f202ce23b7e39d8
Terence Lee <tklee3@illinois.edu>
2022-12-04 09:58:20 -0600

    added delay for speak

--- SlashCommands/speak.cs
+++ SlashCommands/speak.cs
@@ -12,8 +12,13 @@ public class WAHAHA : InteractionModuleBase
     public async Task WAHAHA([Summary(description: "Channel to type in")] ITextChannel channel,
      [Summary(description: "Message to include")] string msg,
      [Summary(description: "TTS? (T/F)")] string tts,
-     [Summary(description: "Reply msg ID")] string replyID)
+     [Summary(description: "Reply msg ID")] string replyID,
+     [Summary(description: "Delay (in minutes, enter 0 if no delay)")] int length)
     {
+        if(length > 0)
+        {
+            await Task.Delay(length * 60000); 
+        }
         if(replyID == null)
         {
             if(tts.ToLower() = "t") // check whether to tts msg
@@ -37,7 +42,27 @@ public class WAHAHA : InteractionModuleBase
             }
         }
         
-        await RespondAsync($"Sent msg in {channel.Mention} "); //update user
+        await RespondAsync($"Sent msg in {channel.Mention} after {length} minutes"); //update user
+    }
 
+}
+
+
+public class Reminder : InteractionModuleBase
+{
+    [SlashCommand("Reminder", "Send a reminder in a specified channel")]
+    [EnabledInDm(false)]
+    [DefaultMemberPermissions(GuildPermission.ModerateMembers)]
+    public async Task WAHAHA([Summary(description: "Channel to type in")] ITextChannel channel,
+     [Summary(description: "Message to include")] string msg,
+     [Summary(description: "Delay (minutes)")] int length)
+    {
+        if(length < 0)   // edge cases
+        {
+            throw new Exception("Invalid length");
+        }
+        await Task.Delay(length * 60000);  
+        var messageId = channel.SendMessageAsync(msg, true);
+        await RespondAsync($"Sent msg in {channel.Mention} after {length} minutes"); //update user
     }
 }
