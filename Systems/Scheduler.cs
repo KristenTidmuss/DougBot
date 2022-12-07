5539ebdd5aeeac9e8ac288fcbb85131827f5b7f9
Kristen Tidmuss <N0961364@my.ntu.ac.uk>
2022-12-07 15:37:02 +0000

    Added forum cleanup to scheduler

--- Systems/Scheduler.cs
+++ Systems/Scheduler.cs
@@ -19,7 +19,18 @@ public class Scheduler
             Enabled = true
         };
         timerMinute.Elapsed += Minute;
+        var timerHour = new Timer
+        {
+            Interval = 3600000,
+            Enabled = true
+        };
+        timerHour.Elapsed += Hour;
         Console.WriteLine("Scheduler System Initialized");
+    }
+
+    private async void Hour(object? sender, ElapsedEventArgs e)
+    {
+        await CleanForums(_Client);
     }
 
     private async void Minute(object? sender, ElapsedEventArgs e)
@@ -35,7 +46,7 @@ public class Scheduler
                 {
                     case "RemoveRole":
                         var para = queue.Data.Split(';');
-                        await RemoveRole(ulong.Parse(para[0]), ulong.Parse(para[1]), ulong.Parse(para[2]));
+                        await RemoveRole(_Client, ulong.Parse(para[0]), ulong.Parse(para[1]), ulong.Parse(para[2]));
                         break;
                 }
                 Queue.Remove(queue);
@@ -47,12 +58,34 @@ public class Scheduler
         }
     }
 
-    private async Task RemoveRole(ulong guidID, ulong userID, ulong roleID)
+    private async Task RemoveRole(DiscordSocketClient client, ulong guidID, ulong userID, ulong roleID)
     {
-        var guild = _Client.GetGuild(guidID);
+        var guild = client.GetGuild(guidID);
         var user = guild.GetUser(userID);
         var role = guild.GetRole(roleID);
         if(user == null){return;}
         await user.RemoveRoleAsync(role);
+    }
+
+    private async Task CleanForums(DiscordSocketClient client)
+    {
+        var settings = Setting.GetSettings();
+        //Get forums from client
+        var guild = client.Guilds.FirstOrDefault(g => g.Id.ToString() == settings.guildID);
+        var forums = guild.Channels.Where(c => c.GetType().Name == "SocketForumChannel");
+        //Loop the forums checking for posts inactive for 2 days or more
+        foreach (SocketForumChannel forum in forums)
+        {
+            var threads = await forum.GetActiveThreadsAsync();
+            var forumThreads = threads.Where(t => t.ParentChannelId == forum.Id);
+            foreach (var thread in forumThreads)
+            {
+                var message = await thread.GetMessagesAsync(1).FlattenAsync();
+                if(message.First().Timestamp.UtcDateTime < DateTime.UtcNow.AddDays(-2))
+                {
+                    await thread.ModifyAsync(t => t.Archived = true);
+                }
+            }
+        }
     }
 }
